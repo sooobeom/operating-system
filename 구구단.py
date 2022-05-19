226a5c49ba8a16eaa93a4bc3a15f97aae7cda117
sooobeom <105835030+sooobeom@users.noreply.github.com>
2022-05-19 17:04:23 +0900

    Delete 구구단.py

--- 구구단.py
+++
@@ -1,6 +0,0 @@
-print("★ 구구단을 출력합니다. ★\n")
-for x in range(2, 10):
-    print("------- [" + str(x) + "단] -------")
-    for y in range(1, 10):
-        print(x, "X", y, "=", x*y)
-print("---------------------")
